e928acb63cd515d08e18337c144b40f359afdd2b
MarthQ <martinez34950@gmail.com>
2026-01-30 22:45:59 -0300

    Initial commit of the project, basic movement with placeholders.

---
+++ player_input_actions.gd
@@ -0,0 +1,7 @@
+class_name PlayerInputActions
+extends Resource
+
+@export var left: StringName = "P1 Left"
+@export var right: StringName = "P1 Right"
+@export var up: StringName = "P1 Up"
+@export var down: StringName = "P1 Down"
